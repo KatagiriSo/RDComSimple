32d2e1fd027952b28a17bdde4fe2f430c7f8209c
Katagiri <katagiri@plala.co.jp>
2016-01-08 13:04:46 +0900

    Initial Commit

---
+++ RDCameraSimple/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RDCameraSimple
+//
+//  Created by 片桐奏羽 on 2016/01/08.
+//  Copyright (c) 2016年 SoKatagiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
