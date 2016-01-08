2c8a95e1064b01c4fd19a3a14cafa635d026eb6c
Katagiri <katagiri@plala.co.jp>
2016-01-08 13:49:01 +0900

    first commit

--- RDCameraSimple/ViewController.swift
+++ RDCameraSimple/ViewController.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    let session = AVCaptureSession()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let video = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        let input = AVCaptureDeviceInput(device: video, error: nil)
+        
+        if (session.canAddInput(input)) {
+            session.addInput(input)
+        }
+        
+        let selector = Selector("runtimeError:"), name = AVCaptureSessionRuntimeErrorNotification
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: selector, name:name , object: nil)
+        
+        // preview
+        let prevLayer = AVCaptureVideoPreviewLayer.layerWithSession(session) as! AVCaptureVideoPreviewLayer
+        prevLayer.frame = self.view.bounds
+        self.view.layer.addSublayer(prevLayer)
+        
+        session.startRunning()
+        
     }
 
+    func runtimeError(notification:NSNotification)
+    {
+        if (session.running) {
+            println("session is runnning")
+        } else {
+            println("session is not runnning")
+        }
+        if (session.interrupted) {
+            println("session is interrupted")
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
